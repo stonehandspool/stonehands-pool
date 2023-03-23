4d97635b6ec9fcdc33f14eb017a19c593053baec
DESKTOP-GMFMAME\rfand <rfandl@outlook.com>
2023-03-22 21:05:45 -0400

    maybe better picksheet

--- README.md
+++ README.md
@@ -27,12 +27,14 @@ TODO:
 - Get the picksheet to factor in if a user has submitted picks already or if they're still in the survivor pool (DONE -not going to do resubmissions for now)
     - Also update to disable any picks for the margin pool/survivor pool (DONE)
 - Check the ability to do partial pick sheets and allow for submissions on Thurs and Fri
-- Add a Hall of Fame
 - Update the processWeek script to handle a player who didn't submit
 - Write instructions for what to do to make it all work (e.g. what to do on Monday morning, Tuesday morning, etc.)
 - Actually get this online and double check make sure things work
 - Do a dry run to make sure that the process works
 - Get automated emails working
+
+TODOs for year 2:
+- Add a Hall of Fame
 
 Maybe TODOs:
 - Add in the ability to sign in and sign out (maybe)
--- src/components/picksheet/MatchupCard.tsx
+++ src/components/picksheet/MatchupCard.tsx
@@ -29,8 +29,8 @@ function MatchupCard(props: MatchupCardProps) {
     };
 
     return (
-        <div className='columns is-vcentered'>
-            <div className='column control is-vertical-center'>
+        <div className='container'>
+            <div className='control is-vertical-center'>
                 {<AwayLogo size={45} />}
                 <label htmlFor={`matchup-${matchupNumber}-away-team`} className={chosenTeam === awayTeam ? 'chosen' : ''}>
                     <input
@@ -44,20 +44,7 @@ function MatchupCard(props: MatchupCardProps) {
                     {` ${awayTeamInfo.displayName} (${awayTeamInfo.wins}-${awayTeamInfo.losses}-${awayTeamInfo.ties})`}
                 </label>
             </div>
-            <div className='column control is-narrow is-vertical-center'>
-                <label htmlFor={`matchup-${matchupNumber}-tie`} className={chosenTeam === 'Tie' ? 'chosen' : ''}>
-                    <input
-                        type='radio'
-                        id={`matchup-${matchupNumber}-tie`}
-                        name={`matchup-${matchupNumber}`}
-                        value='Tie'
-                        checked={chosenTeam === 'Tie'}
-                        onChange={onChoiceChange}
-                    />
-                    Tie
-                </label>
-            </div>
-            <div className='column control is-vertical-center'>
+            <div className='control is-vertical-center'>
                 {<HomeLogo size={45} />}
                 <label htmlFor={`matchup-${matchupNumber}-home-team`} className={chosenTeam === homeTeam ? 'chosen' : ''}>
                     <input
@@ -72,6 +59,49 @@ function MatchupCard(props: MatchupCardProps) {
                 </label>
             </div>
         </div>
+        // <div className='columns is-vcentered'>
+        //     <div className='column control is-vertical-center'>
+        //         {<AwayLogo size={45} />}
+        //         <label htmlFor={`matchup-${matchupNumber}-away-team`} className={chosenTeam === awayTeam ? 'chosen' : ''}>
+        //             <input
+        //                 type='radio'
+        //                 id={`matchup-${matchupNumber}-away-team`}
+        //                 name={`matchup-${matchupNumber}`}
+        //                 value={awayTeam}
+        //                 checked={chosenTeam === awayTeam}
+        //                 onChange={onChoiceChange}
+        //             />
+        //             {` ${awayTeamInfo.displayName} (${awayTeamInfo.wins}-${awayTeamInfo.losses}-${awayTeamInfo.ties})`}
+        //         </label>
+        //     </div>
+        //     <div className='column control is-narrow is-vertical-center'>
+        //         <label htmlFor={`matchup-${matchupNumber}-tie`} className={chosenTeam === 'Tie' ? 'chosen' : ''}>
+        //             <input
+        //                 type='radio'
+        //                 id={`matchup-${matchupNumber}-tie`}
+        //                 name={`matchup-${matchupNumber}`}
+        //                 value='Tie'
+        //                 checked={chosenTeam === 'Tie'}
+        //                 onChange={onChoiceChange}
+        //             />
+        //             Tie
+        //         </label>
+        //     </div>
+        //     <div className='column control is-vertical-center'>
+        //         {<HomeLogo size={45} />}
+        //         <label htmlFor={`matchup-${matchupNumber}-home-team`} className={chosenTeam === homeTeam ? 'chosen' : ''}>
+        //             <input
+        //                 type='radio'
+        //                 id={`matchup-${matchupNumber}-home-team`}
+        //                 name={`matchup-${matchupNumber}`}
+        //                 value={homeTeam}
+        //                 checked={chosenTeam === homeTeam}
+        //                 onChange={onChoiceChange}
+        //             />
+        //             {` ${homeTeamInfo.displayName} (${homeTeamInfo.wins}-${homeTeamInfo.losses}-${homeTeamInfo.ties})`}
+        //         </label>
+        //     </div>
+        // </div>
     );
 }
 
