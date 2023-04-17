fca3a391cf8080980457deb04d98af90febb8520
DESKTOP-GMFMAME\rfand <rfandl@outlook.com>
2023-04-17 19:21:23 -0400

    made navbar usable on mobile at least

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,33 @@
 import { Link } from 'react-router-dom';
 import { CURRENT_WEEK } from '../constants';
+import { useState } from 'react';
 
 function NavBar() {
+    const [burgerState, setBurgerState] = useState(false);
+
+    const onBurgerClick = () => {
+        setBurgerState(!burgerState);
+    };
+
     return (
         <nav className='navbar is-primary' role='navigation' aria-label='main navigation'>
             <div className='navbar-brand ml-6'>
                 <a className='navbar-item' href='/'>
                     <h3 className='title is-4 has-text-white stonehands-hero'>STONEHANDS</h3>
                 </a>
+                <a
+                    role='button' 
+                    className={`navbar-burger ${burgerState ? 'is-active' : ''}`}
+                    aria-label='menu' 
+                    aria-expanded='false' 
+                    onClick={onBurgerClick}
+                >
+                    <span aria-hidden='true'></span>
+                    <span aria-hidden='true'></span>
+                    <span aria-hidden='true'></span>
+                </a>
             </div>
-            <div className='navbar-menu mr-6'>
+            <div className={`navbar-menu mr-6 ${burgerState ? 'is-active' : ''}`}>
                 <div className='navbar-end'>
                     <Link className='navbar-item' to='/about'>About</Link>
                     <Link className='navbar-item' to='/picksheet'>Picksheet</Link>
