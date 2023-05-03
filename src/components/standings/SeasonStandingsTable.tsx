e6b44cad99f2cc7cfa7577f3a7231af76bdf2c4d
DESKTOP-GMFMAME\rfand <rfandl@outlook.com>
2023-05-02 21:16:25 -0400

    almost ready for the jazzy test, just need a few more sign ups

--- src/components/standings/SeasonStandingsTable.tsx
+++ src/components/standings/SeasonStandingsTable.tsx
@@ -1,4 +1,4 @@
-import * as seasonStandings from '../../../data/2022/players.json';
+import * as seasonStandings from '../../../data/2023/players.json';
 import { useNavigate } from 'react-router-dom';
 
 type TableColumns = {
