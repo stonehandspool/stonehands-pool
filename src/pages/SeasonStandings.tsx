8ff6ae51828d65d78d826089c961e0ee562c7d25
DESKTOP-GMFMAME\rfand <rfandl@outlook.com>
2025-08-28 09:02:23 -0400

    ready for the 2025 season!

--- src/pages/SeasonStandings.tsx
+++ src/pages/SeasonStandings.tsx
@@ -1,30 +1,13 @@
-import { useState } from 'react';
 import SeasonStandingsTable from '../components/standings/SeasonStandingsTable';
 
-import { CURRENT_YEAR, CURRENT_WEEK, SEASON_READY } from '../constants';
+import { CURRENT_YEAR, CURRENT_WEEK } from '../constants';
 
 function SeasonStandings() {
-  const [notificationVisible, setNotificationVisible] = useState(SEASON_READY);
-
-  const removeNotification = () => {
-    setNotificationVisible(false);
-  };
-
   return (
     <section className="section">
       <div className="container">
         <h1 className="title has-text-centered">{CURRENT_YEAR} Stonehands Pool</h1>
         <h2 className="subtitle has-text-centered">Season standings as of week {CURRENT_WEEK}</h2>
-        {notificationVisible && CURRENT_WEEK <= 4 && (
-          <div className="columns is-centered">
-            <div className="column is-half">
-              <div className="notification is-info is-narrow">
-                <button className="delete" onClick={removeNotification}></button>
-                If you'd like to see a players in-depth season stats you can click on their name!
-              </div>
-            </div>
-          </div>
-        )}
         <SeasonStandingsTable />
       </div>
     </section>
