094cdd802bf9b69408ca28982d2547cb47cf414b
DESKTOP-GMFMAME\rfand <rfandl@outlook.com>
2026-03-19 12:37:42 -0400

    woops, too soon for that

--- src/pages/MarchMadnessAwards.tsx
+++ src/pages/MarchMadnessAwards.tsx
@@ -1,5 +1,5 @@
 import { CURRENT_YEAR } from '../constants';
-import accolades from '../../data/2026/marchmadness/accolades.json';
+import accolades from '../../data/2025/marchmadness/accolades.json';
 
 interface AccoladeInfo {
   id: string;
